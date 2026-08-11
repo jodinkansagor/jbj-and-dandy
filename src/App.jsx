060c02739aaf92eafc4b4b5ab473a581cb23827d
jodinkansagor <joedvice@gmail.com>
2026-08-11 14:41:48 -0700

    donations links

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,44 @@ function App() {
   return (
     <div className="mainInvite">
       <img src={inviteImage} className="inviteImage" />
-      {/* <hr className="line" /> */}
       <div className="rightColumn">
-        <h2>A few things:</h2>
+        <h2>We’re making it official!
+
+        </h2>
+        <h3>
+After a lucky 13 years, we are tying the knot and would love for you all to celebrate with us.
+</h3>
+      <hr className="line" />
         <ul className="listOfStuff">
-          <li>Our home will be open and ready for you all from 11am to 3pm. We imagine we will have our sweet ceremony around noon, but come when you can!
+          <li>Our home will be open for you all from 11am to 3pm. Brunch starts at 11am, ceremony at 12pm, cake at 1pm, and piñata at 2pm.
           </li>
-          <li>We will have food and drinks for everyone. I think we are pretty familiar with most people’s food needs, but let us know if you think otherwise
+          <li>
+            There will be food and drinks for everyone. We are familiar with people’s dietary needs. Let us know if you have any concerns.
           </li>
-          <li>Please do a rapid covid test before coming and if you aren’t feeling well, please stay home. We would love to celebrate with you when you are feeling better.
+          <li>Please do a rapid covid test before coming. If you aren’t feeling well, please stay home. We would love to celebrate with you when you are feeling better
+
+          </li>
+          <li>
+            We have carefully curated a guest list of our nearest and dearest. To keep this small and intimate, there will be no plus ones. We love you all and are looking forward to sharing food, drinks, and fun times with you!
+
+          </li>
+          <li>
+            As part of the ceremony, we are asking folks to share a 2-3 minute story or favorite memory as you have witnessed our relationship grow over the past 13 years. Participation is optional.
+
           </li>
         </ul>
         <hr className="line" />
-        <h2>
+        <h3>
           In lieu of gifts for us, you can make a gift to any of the following organizations.
-        </h2>
+        </h3>
         <div className="orgs">
 
-        <a href="">Org 1</a>
-        <a href="">Org 2</a>
+          <a href="https://northwestdogproject.org/donate" target="_blank">Northwest Dog Project</a>
+          <a href="https://www.zeffy.com/en-US/donation-form/the-confederated-tribes-of-the-warm-springs-reservation-of-oregon-ctws-wildfire-recovery-fund" target="_blank">Warm Springs Wildfire Recovery</a>
+          <a href="https://opencollective.com/the-sameer-project" target="_blank">The Sameer Project Palestinian Mutual Aid</a>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
