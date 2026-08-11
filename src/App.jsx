cbd8f327a64a0d0ce938009ff2f05227a41e3d9b
jodinkansagor <joedvice@gmail.com>
2026-08-10 19:15:23 -0700

    colors, sizing

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="mainInvite">
       <img src={inviteImage} className="inviteImage" />
-      <hr className="line" />
-
-      <h2>A few things:</h2>
+      {/* <hr className="line" /> */}
+      <div className="rightColumn">
+        <h2>A few things:</h2>
         <ul className="listOfStuff">
           <li>Our home will be open and ready for you all from 11am to 3pm. We imagine we will have our sweet ceremony around noon, but come when you can!
           </li>
@@ -17,6 +17,16 @@ function App() {
           <li>Please do a rapid covid test before coming and if you aren’t feeling well, please stay home. We would love to celebrate with you when you are feeling better.
           </li>
         </ul>
+        <hr className="line" />
+        <h2>
+          In lieu of gifts for us, you can make a gift to any of the following organizations.
+        </h2>
+        <div className="orgs">
+
+        <a href="">Org 1</a>
+        <a href="">Org 2</a>
+        </div>
+      </div>
     </div>
   )
 }
